6b69d107d9b057e93c603ebf0e03fef3e4dc67aa
lawrence <lodhiambo26@gmail.com>
2024-01-15 09:37:22 +0000

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,10 +6,12 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n():
+async def wait_n(n: int, max_delay: int = 10) -> list:
     '''
     should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency.
     '''
-
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
