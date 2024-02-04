c134144c909ed3c1527c13805910d7beade5ddf2
lawrence <lodhiambo26@gmail.com>
2024-02-04 17:02:30 +0000

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -59,7 +59,7 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
         ])
-
+    
     def test_get_json(
             self,
             test_url: str,
